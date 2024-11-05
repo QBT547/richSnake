fe0674ad6eb876bc37831c43a19d02f2eb6ea94c
leen2233 <arslanrejepow223@gmail.com>
2024-11-05 18:17:25 +0500

    moved bot token to top

--- richSnake/settings.py
+++ richSnake/settings.py
@@ -13,7 +13,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+import os
+from dotenv import load_dotenv
 
+# Load the .env file
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -145,11 +150,3 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-
-import os
-from dotenv import load_dotenv
-
-# Load the .env file
-load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
